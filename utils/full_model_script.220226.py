d6eb86b2336cc1ef60f5ed8f7e80d13621db3d5a
ekim <i.ekimcelikay@gmail.com>
2026-02-24 13:37:21 +0100

    deleted the multirun or argparse scripts from dipc. moved test scripts to tests folder (not for pytest). adding produced_sequences to the repo for ease of access.

---
+++ utils/full_model_script.220226.py
@@ -0,0 +1,8 @@
+# dipc de calistircagim model functionini yaziyorum
+# cochlear output -mean psth
+# sonra bu exponential'a giriyo
+# sonra duration function'i: bi gaussian olcak tau parametrreli
+# adaptrans modeli: burdaki tau vs bilmemne parametreler
+# bunlar ucu carpilcak
+
+# tamam once dipc'den cochlear model almaya calisim
